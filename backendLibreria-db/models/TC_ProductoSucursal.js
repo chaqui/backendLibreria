9e1e386281a5d869bd3c28d0037213f1a0224e44
Josue Chaqui <josue.chaqui@gmail.com>
2018-09-04 12:51:22 -0600

    eliminando elementos innecesario de TC_ProductoSucursal

--- backendLibreria-db/models/TC_ProductoSucursal.js
+++ backendLibreria-db/models/TC_ProductoSucursal.js
@@ -6,15 +6,6 @@ const setupDatabase = require('../lib/db')
 module.exports = function setupTC_ProductoSucursal (config){
     const  Sequelize = setupDatabase(config)
     return Sequelize.define('TC_ProductoSucursal',{
-        stok:
-        {
-            type:Sequelize.INTEGER,
-            allowNull:false
-        },
-        precio:{
-            type:Sequelize.DOUBLE,
-            allowNull:false
-        },
         alarma:{
             type:Sequelize.DOUBLE,
             allowNull:false
