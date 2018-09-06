020ee0bc683cc31ae20e95419e541a67d9c0c381
Josue Chaqui <josue.chaqui@gmail.com>
2018-09-06 14:18:41 -0600

    agregando stubs

---
+++ backendLibreria-db/tests/stubs.js
@@ -0,0 +1,121 @@
+'use strict'
+
+module.exports = function stubs () {
+  let tCUsuarioSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tCClienteSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tCRepresentanteSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tcControlSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTCajaTiendaSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tCCategoriaSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tCMarcaSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tCProductoSucursalSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tCProveedorStub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tCServicioSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tCSucursalSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tCVendedorSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTdescripcionVentaSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tTegresosSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  //
+  let tTenvioASucursalSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTingresosSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTproductoVendidoSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTSaldoADeberSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTSaldoPagadoSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+  let tTServicioVendidoSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  let tTVentaSub = {
+    hasMany: function () {},
+    belongsTo: function () {}
+  }
+
+  return {
+    tCUsuarioSub,
+    tCClienteSub,
+    tCRepresentanteSub,
+    tcControlSub,
+    tTCajaTiendaSub,
+    tCCategoriaSub,
+    tCMarcaSub,
+    tCProductoSucursalSub,
+    tCProveedorStub,
+    tCServicioSub,
+    tCSucursalSub,
+    tCVendedorSub,
+    tTdescripcionVentaSub,
+    tTegresosSub,
+    tTenvioASucursalSub,
+    tTingresosSub,
+    tTproductoVendidoSub,
+    tTSaldoADeberSub,
+    tTSaldoPagadoSub,
+    tTServicioVendidoSub,
+    tTVentaSub
+  }
+}
